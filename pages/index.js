e85d7a30a25eccce0ebc9c9734a37163ee4b38d2
Axely7 <axely7@hotmail.com>
2022-11-08 00:18:03 -0600

    project init

---
+++ pages/index.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>Axel Jimenez</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <section className="bg-blue-700"></section>
+      </main>
+    </div>
+  );
+}
