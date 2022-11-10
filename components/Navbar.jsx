21dbe7759d43f8bfa6be920e7d97f1cb294cbbce
Axely7 <axely7@hotmail.com>
2022-11-10 00:45:02 -0600

    navbar

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
+import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 import React from "react";
 
 export const Navbar = () => {
@@ -13,9 +14,44 @@ export const Navbar = () => {
           height="50"
         />
         <div>
-          <ul>
-            <Link href="/">Home</Link>
+          <ul className="hidden md:flex">
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Home</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">About</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">Skills</li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Projects
+              </li>
+            </Link>
+            <Link href="/">
+              <li className="ml-10 text-sm uppercase hover:border-b">
+                Contact
+              </li>
+            </Link>
           </ul>
+          <div className="md:hidden">
+            <AiOutlineMenu size={25} />
+          </div>
+        </div>
+      </div>
+      <div className="fixed left-0 top-0 w-full h-screen bg-black/70">
+        <div className="fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-[#ecf0f3] p-10 ease-in duration-500">
+          <div>
+            <div>
+              <Image
+                src="/../public/assets/navLogo.png"
+                alt="/"
+                width="87"
+                height="35"
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
