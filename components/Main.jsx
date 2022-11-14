b778ab53118ab96f3892178dd2044e051baae733
Axely7 <axely7@hotmail.com>
2022-11-14 00:36:12 -0600

    navbar done main page working

---
+++ components/Main.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+export const Main = () => {
+  return (
+    <div className="w-full h-screen text-center">
+      <div className="max-w-[1240px] w-full h-full mx-auto p-2 flex justify-center items-center">
+        <div>
+          <p className="uppercase text-sm tracking-widest text-gray-600">
+            LET'S BUILD SOMETHING TOGETHER
+          </p>
+          <h1 className="py.4 text-gray-700">
+            Hi, I'm <span className="text-[#5651e5]">Axel</span>
+          </h1>
+          <h1 className="py-4 text-gray-700">A Front-End Web Developer</h1>
+        </div>
+      </div>
+    </div>
+  );
+};
